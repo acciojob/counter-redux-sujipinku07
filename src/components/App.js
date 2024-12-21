fc820283a6f494e4fa0b3beaf904ad91d272b863
sujitha <sujipinku07@gmail.com>
2024-12-21 22:20:42 +0530

    counter-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
 import './../styles/App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {increment,decrement} from "../redux/action/counterAction";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App =() => {
+    const val=useSelector(state=>state.count);
+    const dispatch=useDispatch();
+
+
+    return (
+        <div>
+            <h1>{val}</h1>
+            <button onClick ={()=> dispatch(increment())}>increment</button>
+            <button onClick ={()=> dispatch(decrement())}>decrement</button>
+        </div>
+    )
 }
-
-export default App
+export default App;
